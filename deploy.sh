df196bcea11f8b830f84d0b507608c8b4c044fde
Jonas Gröger <jonas.groeger@gmail.com>
2015-08-25 21:29:08 +0200

    Also add .svg

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p ~/Dropbox/Studium/Master/InfoVis/Übungsabgaben/5-Uebung/pyDNA/
-cp dna.py generate.sh ~/Dropbox/Studium/Master/InfoVis/Übungsabgaben/5-Uebung/pyDNA/
+cp dna.py generate.sh *.svg ~/Dropbox/Studium/Master/InfoVis/Übungsabgaben/5-Uebung/pyDNA/
 
 echo "Done."
